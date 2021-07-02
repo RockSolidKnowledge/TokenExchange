4da31367434213c8df54f531ede11f4c23338b68
Scott Brady <scott@scottbrady91.com>
2021-07-02 08:44:40 +0100

    Added missing comments

--- src/Rsk.TokenExchange.DuendeIdentityServer/IdentityServerBuilderExtensions.cs
+++ src/Rsk.TokenExchange.DuendeIdentityServer/IdentityServerBuilderExtensions.cs
@@ -4,8 +4,15 @@ using Rsk.TokenExchange.Validators.Adaptors;
 
 namespace Rsk.TokenExchange.DuendeIdentityServer
 {
+    /// <summary>
+    /// Token exchange registrations for IIdentityServerBuilder
+    /// </summary>
     public static class IdentityServerBuilderExtensions
     {
+        /// <summary>
+        /// Adds the default token exchange registrations for Duende IdentityServer.
+        /// Uses the default authorization policy, designed for token exchange between microservices. 
+        /// </summary>
         public static IIdentityServerBuilder AddTokenExchange(this IIdentityServerBuilder builder)
         {
             builder.Services.AddTokenExchangeCore();
--- src/Rsk.TokenExchange.IdentityServer4/IdentityServerBuilderExtensions.cs
+++ src/Rsk.TokenExchange.IdentityServer4/IdentityServerBuilderExtensions.cs
@@ -4,8 +4,15 @@ using Rsk.TokenExchange.Validators.Adaptors;
 
 namespace Rsk.TokenExchange.IdentityServer4
 {
+    /// <summary>
+    /// Token exchange registrations for IIdentityServerBuilder
+    /// </summary>
     public static class IdentityServerBuilderExtensions
     {
+        /// <summary>
+        /// Adds the default token exchange registrations for IdentityServer4.
+        /// Uses the default authorization policy, designed for token exchange between microservices.
+        /// </summary>
         public static IIdentityServerBuilder AddTokenExchange(this IIdentityServerBuilder builder)
         {
             builder.Services.AddTokenExchangeCore();
